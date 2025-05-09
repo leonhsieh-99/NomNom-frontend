67ec5f197a231bc02b35bf987e34b5aa58c417fd
Leon Hsieh <leonhsieh@Leons-MacBook-Pro-2.local>
2025-05-08 23:54:44 -0700

    fixed api call

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import type { Restaurant } from "../types";
 
+const API_BASE = import.meta.env.VITE_BACKEND_URL;
+
 export async function fetchRandomRestaurant(queryString = ''): Promise<Restaurant> {
-    const response = await fetch(`/api/random_restaurant?${queryString}`);
+    const response = await fetch(`${API_BASE}/api/random_restaurant?${queryString}`);
     const data = await response.json();
 
     if (!response.ok || data.error) {
